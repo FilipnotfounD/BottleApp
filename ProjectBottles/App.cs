db588a6cd2669fd00edb36fa07b38a4967791a4e
1107 <1107@student.itgymnazium.cz>
2024-04-22 09:30:09 +0200

    Second

---
+++ ProjectBottles/App.cs
@@ -0,0 +1,68 @@
+﻿namespace ProjectBottles;
+
+public class App
+{
+    public void Run()
+    {
+        bool proceed = true;
+
+        Console.WriteLine("Zadej maximální objem lahve (v mililitrech):");
+        var answer = SafelyConvertToInt(Console.ReadLine()!);
+
+        Console.WriteLine("Zadej výrobce lahve:");
+        var answer2 = Console.ReadLine()!;
+
+        var Bottle = new Bottle(answer, answer2);
+        Bottle.CurrentVolume = answer;
+
+        while (proceed)
+        {
+            Console.WriteLine("Zadej svůj požadavek: Doplnit lahev (D) / Snížit objem o 50ml (S) / Snížit objem o zadaný objem (Z) / Ukončit celý proces (U)");
+            var answer3 = Console.ReadLine()!;
+
+            if (answer3 == "D")
+            {
+                Console.WriteLine(Bottle.Refill());
+                PrintDeLimiter();
+            }
+            else if(answer3 == "S")
+            {
+                Console.WriteLine(Bottle.Sip());
+            }
+            else if(answer3 == "Z")
+            {
+                PrintDeLimiter();
+                Console.WriteLine("Zadej kolik o kolik chceš snížit objem:");
+                int answer4 = SafelyConvertToInt(Console.ReadLine()!);
+
+                Console.WriteLine(Bottle.Sip(answer4));
+                PrintDeLimiter();
+            }
+            else if(answer3 == "U")
+            {
+                proceed = false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    public int SafelyConvertToInt(string input)
+    {
+        if (int.TryParse(input, out int result))
+        {
+            return result;
+        }
+
+        else
+        {
+            return int.MinValue;
+        }
+    }
+    public void PrintDeLimiter()
+    {
+        Console.WriteLine("---------------------------");
+    }
+}
---
+++ ProjectBottles/Bottle.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectBottles;
+
+public class Bottle
+{
+    public int MaxVolume;
+    public int CurrentVolume;
+    public string Maker { get; set; }= null!;
+
+    public Bottle(int MaxObjem, string Vyrobce)
+    {
+        MaxVolume = MaxObjem;
+
+        Maker = Vyrobce;
+    }
+
+    public bool Sip()
+    {
+        if (CurrentVolume >= 50)
+        {
+            CurrentVolume -= 50;
+
+            return true;
+        }
+
+        else
+        {
+
+            return false;
+
+        }
+    }
+
+    public string Sip(int input)
+    {
+
+        if(CurrentVolume < input)
+        {
+            var value = CurrentVolume;
+
+            CurrentVolume = 0;
+
+            return $"Bylo ubráno: {value} ml.";
+        }
+
+        else
+        {
+            CurrentVolume -= input;
+
+            return $"Bylo ubráno: {input} ml.";
+        }
+    }
+
+    public string Refill()
+    {
+        var rozdil = MaxVolume - CurrentVolume;
+
+        CurrentVolume += rozdil;
+
+        return $"Bylo doplněno: {rozdil} ml.";
+    }
+}
